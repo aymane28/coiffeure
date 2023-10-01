3ac7d274bc974c31e0bef43f54e02bf7faaf347d
Aymane Afdari <aymanedhj7@gmail.com>
2023-10-01 22:32:37 +0200

    Change color button choice date/time

--- src/Controller/User/ProfilSettings.php
+++ src/Controller/User/ProfilSettings.php
@@ -4,17 +4,21 @@ namespace App\Controller\User;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Test\Constraint\ResponseStatusCodeSame;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilSettings extends AbstractController
 {
     #[Route("/settings", name: "profil_settings")]
-    public function view(): Response
+    public function view(SessionInterface $session): Response
     {
+
+        $cart = $session->get('cart', []);
 
         return $this->render('user/profilsettings.html.twig', [
             'profil_settings' => 'ProfilSettings',
+            'cart' => $cart
         ]);
     }
 }
